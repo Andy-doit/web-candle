18cb54377ccc88cbd2ab8ffefb6c901e69ee772c
Andy-doit <anlee221202@gmail.com>
2026-02-11 22:09:04 +0700

    update scroll

--- src/App.tsx
+++ src/App.tsx
@@ -1,9 +1,11 @@
-import {BrowserRouter} from "react-router-dom";
+import { BrowserRouter } from "react-router-dom";
 import Main from "./views/Main.tsx";
+import { ScrollToTop } from "./components";
 
 function App() {
   return (
     <BrowserRouter>
+      <ScrollToTop />
       <Main></Main>
     </BrowserRouter>
   )
--- src/components/index.tsx
+++ src/components/index.tsx
@@ -1,6 +1,7 @@
-export {default as MissCandleButton} from './MissCandleButton.tsx';
-export {default as MissCandleFooter} from './MissCandleFooter';
-export {default as MissCandleHeader} from './MissCandleHeader';
-export {default as MissCandleProductCard} from './MissCandleProductCard';
-export {default as MissCandleSpecialCollection} from './MissCandleSpecialCollection';
-export {default as MissCandleValueProp} from './MissCandleValueProp';
+export { default as MissCandleButton } from './MissCandleButton.tsx';
+export { default as MissCandleFooter } from './MissCandleFooter';
+export { default as MissCandleHeader } from './MissCandleHeader';
+export { default as MissCandleProductCard } from './MissCandleProductCard';
+export { default as MissCandleSpecialCollection } from './MissCandleSpecialCollection';
+export { default as MissCandleValueProp } from './MissCandleValueProp';
+export { default as ScrollToTop } from './ScrollToTop';
